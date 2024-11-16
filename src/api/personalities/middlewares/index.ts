d5a33255ec59dc9ed7414601b9d90235e1eed1c5
Marco <marcobert37@gmail.com>
2024-11-16 15:46:42 +0100

    fix(personalities): fix delete

--- src/api/personalities/middlewares/index.ts
+++ src/api/personalities/middlewares/index.ts
@@ -2,7 +2,7 @@ import { PersonalityDocument } from "../interfaces";
 import { QuestionTemplates } from "../../question-templates/model";
 
 export async function checkTemplatesBeforeDelete(this: PersonalityDocument, next: Function): Promise<void> {
-    const questionTemplatesCount = await QuestionTemplates.countDocuments({ personality: this._id });
+    const questionTemplatesCount = await QuestionTemplates.countDocuments({ personalityId: this._id });
 
     if (questionTemplatesCount > 0) {
         return next('Cannot delete personality with associated question templates');
